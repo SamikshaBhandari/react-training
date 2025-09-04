727bf49fa2135dd3422e8e828bc822c985fbab60
Samiksha <samikshyabhandari592@gmail.com>
2025-09-04 08:55:36 +0545

    array add

---
+++ javascript/day-4.js
@@ -0,0 +1,12 @@
+const student={
+    name:"Samiksha",
+    age:20,
+    isStudent:true,
+}
+console.log(student);
+student.age=student.age+1;
+console.log(student);
+
+
+let arr=[1,2,3];
+console.log(arr[1]);
